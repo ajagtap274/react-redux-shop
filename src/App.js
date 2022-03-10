a52e0a887d4366fe5f507d801cc10438a75472be
DESKTOP-EIDMFKB\ABHISHEK <ajagtap274@gmail.com>
2022-03-10 23:47:47 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-     Hello Again React!
+      <Header />
+      <Router>
+        
+        <Routes>
+          {/* do not forget the angular brackets aroud component */}
+          <Route path='/' exact element={<ProductListing/>} />
+          <Route path='/product/:productId' exact element={<ProductDetails />} />
+          <Route>404 Not Found </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,29 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initialState = {
+    products: [
+       
+    ]
+}
+
+//action is destructured as {type, payload}
+export const productReducer = (state = initialState, {type, payload}) => {
+    switch(type) {
+        case ActionTypes.SET_PRODUCTS:
+            return {...state, products:payload};
+        default:
+            return state;
+    }
+};
+
+
+export const selectedProductReducer = (state = {}, {type, payload}) => {
+    switch(type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, product:payload};
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
+        default:
+            return state;
+    }
+};
